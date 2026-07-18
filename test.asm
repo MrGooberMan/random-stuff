86e1aa78b19fc5385e293e43d2a138e3164d6537
MrGooberMan <153855541+MrGooberMan@users.noreply.github.com>
2026-07-18 22:51:50 +0000

    Update test.asm

--- test.asm
+++ test.asm
@@ -1,15 +1,14 @@
+MOV r1 #2
+MOV r2 #1
+MOV r3 #8
+MUL r2 r2 r1
+CMP r2 r3
+JC #3 #4
 MOV r1 #5
-MOV r2 #3
 MOV r3 #1
-ADD r0 r1 r2
+ADD r2 r2 r1
 SUB r1 r1 r3
-CMP r1 r3
-JC #2 #4
-MUL r0 r0 r2
-ADD r0 r0 r1
-MOV r3 #2
-MUL r0 r0 r3
-SUB r2 r2 #1
-JC #1 #3
-OUT r0
+CMP r1 #0
+JC #1 #9
+OUT r2
 BRK
